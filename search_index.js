298294a421e43057acca9fd3d2befbbd5224d48f
knarkzel <85593302+knarkzel@users.noreply.github.com>
2022-03-27 16:39:20 +0000

    Deploying to docs from @ knarkzel/feed_forward@8de20df07d35083ca7fb16667e8ef7c835bc4b59 🚀

---
+++ search_index.js
@@ -0,0 +1,4 @@
+var searchModuleIndex = ["",];
+var searchIndex = ["Network","feed","output","mutate","crossover","network",];
+var searchModuleData = [["","index.html"],];
+var searchData = [["","","index.html#Network","struct "],["","","index.html#Network.feed","fn (Network)"],["","","index.html#Network.output","fn (Network)"],["","","index.html#Network.mutate","fn (Network)"],["","","index.html#Network.crossover","fn (Network)"],["","","index.html#network","fn "],];
